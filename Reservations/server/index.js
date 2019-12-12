c461b3a225e92f887dd94d1f95fce510834489f4
Neil Enriquez <neil.dge@gmail.com>
2019-12-11 23:24:34 -0800

    Render Menu and PhotoGallery. Should render Reservations as well with Babel Error fix

--- Reservations/server/index.js
+++ Reservations/server/index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static('client/dist'));
 app.use(router);
 
-let port = 3002;
+let port = 3001;
 
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
